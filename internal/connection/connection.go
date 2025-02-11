869b8e4edff85be1eb96bc7d03ac22526e68d8a3
Bryan Abi Karam <bryancrisso@outlook.com>
2025-02-11 16:31:23 +0000

    feat: add internal connection package and use correct api types

--- internal/connection/connection.go
+++ internal/connection/connection.go
@@ -1,15 +1,17 @@
-package main
+package connection
+
+import "github.com/khorcarol/AgentOfThings/internal/api"
 
 type Config struct {
 	X int
 }
 
 var (
-	NewUserChannel       chan string = make(chan string)
-	FriendRequestChannel chan string = make(chan string)
+	NewUserChannel       chan api.User    = make(chan api.User)
+	FriendRequestChannel chan api.Friends = make(chan api.Friends)
 )
 
-func RequestFriend(ID string, data string) (success bool, receiptData string) {
+func RequestFriend(user api.User, data api.Friend) (success bool, receiptData api.Friend) {
 	// start a new stream with friend request protocol
 	return false, ""
 }
