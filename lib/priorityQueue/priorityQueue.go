9b09569ea4454c329910a97b9bc813c0bea2eb39
Oliver Fitzgerald <ojbfitzgerald@gmail.com>
2025-02-15 00:25:54 +0000

    Fixed prioritieQueue repition 2

--- lib/priorityQueue/priorityQueue.go
+++ lib/priorityQueue/priorityQueue.go
@@ -1,7 +1,8 @@
-package lib
+package priorityQueue
 
 import (
 	"container/heap"
+	"github.com/khorcarol/AgentOfThings/lib/option"
 )
 
 type Item struct {
@@ -10,28 +11,28 @@ type Item struct {
 	index int
 }
 
-type THeap []*Item
+type tHeap []*Item
 
 type PriorityQueue[T any] struct {
-	h THeap
+	h tHeap
 	length int
 }
 
 // Heap functions
-func (h *THeap) Len() int { return len(*h) }
+func (h *tHeap) Len() int { return len(*h) }
 
-func (h *THeap) Less(i int, j int) bool {
+func (h *tHeap) Less(i int, j int) bool {
 	return (*h)[i].priority < (*h)[j].priority
 }
 
-func (h *THeap) Push(x any){
+func (h *tHeap) Push(x any){
 	n := len(*h)
 	item := x.(*Item) 
 	item.index = n
 	*h = append(*h, item)
 }
 
-func (h *THeap) Pop() any{
+func (h *tHeap) Pop() any{
 	old := *h
 	n := len(old)
 	item := old[n-1]
@@ -41,7 +42,7 @@ func (h *THeap) Pop() any{
 	return item.value
 }
 
-func (h THeap) Swap(i int, j int){
+func (h tHeap) Swap(i int, j int){
 	h[i], h[j] = h[j], h[i]
 	h[i].index = i
 	h[j].index = j
@@ -50,9 +51,9 @@ func (h THeap) Swap(i int, j int){
 
 // Priority Queue Functions
 
-func newPriorityQueue[T any](size int) PriorityQueue[T]{
+func NewPriorityQueue[T any](size int) PriorityQueue[T]{
 	
-	h := make(THeap, size)
+	h := make(tHeap, size)
 	pq := PriorityQueue[T]{h, 0}
 
 	heap.Init(&pq.h)
@@ -71,28 +72,24 @@ func (pq PriorityQueue[T]) Push(val T, priority int){
 	heap.Push(&pq.h, item)
 }
 
-func (pq PriorityQueue[T]) Pop() Option[T]{
+func (pq PriorityQueue[T]) Pop() option.Option[T]{
 	if pq.length == 0{
-		return optionNil[T]()
+		return option.OptionNil[T]()
 	} else{
 		pq.length --
-		return optionVal(pq.h.Pop().(T))
+		return option.OptionVal(pq.h.Pop().(T))
 	}
 }
 
-func (pq PriorityQueue[T]) to_list() []T{
+func (pq PriorityQueue[T]) To_list() []T{
 	res := make([]T, pq.Len())
 	cp := pq
 	
 	for i:=0; i<cp.Len(); i++ {
 		val := cp.Pop()
-		if val.set {
-			res[i] = val.val
+		if val.GetSet(){
+			res[i] = val.GetVal()
 		}
 	}
 	return res
-}
-
-func test() int{
-	return 1;
 }
