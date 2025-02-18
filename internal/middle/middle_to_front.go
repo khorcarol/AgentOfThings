e3a40bc0d96346404a4d8811b5d7eadaba73c8df
Oliver Fitzgerald <ojbfitzgerald@gmail.com>
2025-02-18 11:45:21 +0000

    fix: fixed circular import

--- internal/middle/middle_to_front.go
+++ internal/middle/middle_to_front.go
@@ -2,13 +2,12 @@ package middle
 
 import (
 	"github.com/khorcarol/AgentOfThings/internal/api"
-	"github.com/khorcarol/AgentOfThings/internal/middle"
 )
 
 
 // A collection of functions to be used by the front end
 func seen(userID api.ID) {
-	middle.SetUserSeen(userID, true)
+	SetUserSeen(userID, true)
 }
 
 func sendFriendRequest(userID api.ID){
