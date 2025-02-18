10e418dca1cb7926bb620bbcc748d30dd421b490
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-02-18 14:42:12 +0000

    feat(plex): add film thumbnail url for films

--- internal/sources/plex.go
+++ internal/sources/plex.go
@@ -16,8 +16,10 @@ import (
 func GetPlexInterests() []api.Interest {
 	ctx := context.Background()
 
+	server_url := "http://localhost:32400"
+
 	s := plexgo.New(
-		plexgo.WithServerURL("http://localhost:32400"),
+		plexgo.WithServerURL(server_url),
 	)
 
 	res, err := s.Sessions.GetSessionHistory(ctx, nil, nil, nil, nil)
@@ -30,12 +32,18 @@ func GetPlexInterests() []api.Interest {
 		interests_contained := map[string]bool{}
 
 		for _, media := range res.Object.MediaContainer.Metadata {
+			var image_url *string
+			if media.Thumb != nil {
+				thumbnail_url := server_url + *media.Thumb
+				image_url = &thumbnail_url
+			}
 			if media.Key != nil && !interests_contained[*media.Key] {
 				interests_contained[*media.Key] = true
 
 				interests = append(interests, api.Interest{
 					Category:    FilmTV,
 					Description: *media.Title,
+					Image:       image_url,
 				})
 			}
 		}
