38a655e914f1ce7a0ba515717c810069e4d1188f
Oliver Fitzgerald <ojbfitzgerald@gmail.com>
2025-02-20 11:47:16 +0000

    feat (middle/front): Connected frontend to the middle

--- internal/middle/middle_to_front.go
+++ internal/middle/middle_to_front.go
@@ -5,10 +5,19 @@ import (
 )
 
 // A collection of functions to be used by the front end
-func seen(userID api.ID) {
+func Seen(userID api.ID) {
 	SetUserSeen(userID, true)
 }
 
-func sendFriendRequest(userID api.ID) {
+func SendFriendRequest(userID api.ID) {
+	user, ok := users[userID]
+	if (!ok){
+		// TODO: Make Error
+		return
+	}
 
+	delete(users, userID)
+	friend_requests[userID] = user
+
+	// TODO: send user along backend channel
 }
