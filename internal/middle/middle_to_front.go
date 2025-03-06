97bbb464ada8e9d2a4c5ee5d23fcd00f009af043
Bryan Abi Karam <bryancrisso@outlook.com>
2025-03-06 14:30:30 +0000

    feat(middle): clean up friend request/response logic

--- internal/middle/middle_to_front.go
+++ internal/middle/middle_to_front.go
@@ -10,25 +10,25 @@ import (
 
 type FrontEndFunctions struct {
 	friend_refresh func(friends []api.Friend)
-	user_refresh func(users []api.User)
+	user_refresh   func(users []api.User)
+	fr_refresh     func(in []api.User, out []api.User)
 }
 
 var frontend_functions FrontEndFunctions
 
-func Pass(refreshfriends  func(friends []api.Friend), refreshusers func(users []api.User) ){
+func Pass(refreshfriends func(friends []api.Friend), refreshusers func(users []api.User)) {
 	frontend_functions.friend_refresh = refreshfriends
 	frontend_functions.user_refresh = refreshusers
 }
 
 func CommonInterests(userID api.ID) []api.Interest {
-	return common_interests[userID] 
+	return common_interests[userID]
 }
 
 // A collection of functions to be used by the front end
 func Seen(userID api.ID) {
 	setUserSeen(userID, true)
 }
-
 
 func GetFriends() []api.Friend {
 	ret := []api.Friend{}
@@ -56,5 +56,7 @@ func SendFriendRequest(userID api.ID) {
 	delete(users, userID)
 	ranked_users.Remove(userID)
 	friend_requests[userID] = user
-}
 
+	frontend_functions.fr_refresh(getFriendRequests())
+	frontend_functions.user_refresh(getUserList())
+}
